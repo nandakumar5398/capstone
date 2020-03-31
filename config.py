dc21b3b4ef25efd875ac402ec285c0eba2646771
nandakumar5398 <nandakumar5398@gmail.com>
2020-03-31 10:29:27 +0530

    Updated postgres

--- config.py
+++ config.py
@@ -7,5 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connecting to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/capstone'
+
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/capstone'
+SQLALCHEMY_DATABASE_URI = 'postgres://pzlwtqgzcuohsk:d76f3f9f10bc72ad02bf6147b1a52eeb54ba920994ede3419681875a1dcc8334@ec2-18-209-187-54.compute-1.amazonaws.com:5432/dchkpjunb9i0v3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
